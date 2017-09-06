32a693b3d056a38effb54d9c243b2672a05ae3be
tobkle <tobias@klemmer.info>
2017-09-06 23:07:44 +0200

    requiredTypes.conf -> requiredTypes.js

--- src/schema/buildRequiredTypes.js
+++ src/schema/buildRequiredTypes.js
@@ -1,6 +1,8 @@
 import { readString } from '../util/read';
 import path from 'path';
 
+import { requiredTypes } from './requiredTypes';
+
 /**
  * reads and graphql file and returns it as a string
  * @param {string} pathToFile - the path of the required 
@@ -8,8 +10,9 @@ import path from 'path';
  * @return {string} requiredTypes - schema containing the required types
  */
 
-export function buildRequiredTypes(pathToFile: string): any {
+export function buildRequiredTypes(): string {
   // for any reason npm install always forgets to install the .graphql file
-  const filePath = path.join(__dirname, pathToFile || 'requiredTypes.conf');
-  return readString(filePath);
+  // const filePath = path.join(__dirname, pathToFile || 'requiredTypes.conf');
+  // return readString(filePath);
+  return requiredTypes;
 }
